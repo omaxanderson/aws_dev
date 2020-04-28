4ef2d7c2fc1a770882ed7b20bce34503befe92f4
omaxanderson <omaxwellanderson@gmail.com>
2020-04-28 14:00:41 -0500

    fix hostname

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,17 @@
+var child_process = require('child_process');
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', {
-      title: 'Express',
-      host: process.env.HOST,
+  child_process.exec('hostname -f', (err, stdout) => {
+      if (err) {
+          return res.send('ERROR');
+      }
+      res.render('index', {
+          title: 'Express',
+          host: stdout,
+      });
   });
 });
 
